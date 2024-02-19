39abe0abc49b45d57ad00788d4a7c29a436db02d
Steve <ksung91889@naver.com>
2024-02-19 12:00:52 +0900

    feat-10: 페이지네이션 WIP

--- static/videoroom-client.js
+++ static/videoroom-client.js
@@ -655,6 +655,8 @@ socket.on('exists', ({ data }) => {
   console.log('room exists', data);
 });
 
+
+
 socket.on('rooms-list', ({ data }) => {
   // console.log('data >>>>>> ', data); // janus.plugin.videoroom.jcfg 코드 에서 옴.
   // var parsedData = JSON.parse(data);
@@ -662,8 +664,8 @@ socket.on('rooms-list', ({ data }) => {
   $('#room_list').html('');
   data.list.forEach(rooms => { // data.list.forEach는 내꺼 돌아가고, parsedData.forEach는 peter꺼.
     $('#room_list').html($('#room_list').html()+"<br>"+rooms.description +" ("+rooms.num_participants+" / "+rooms.max_publishers+")&nbsp;<button class='btn btn-primary btn-xs' onclick='join22("+rooms.room+", \""+rooms.description+"\");'>join</button>&nbsp;"+"<button class='btn btn-primary btn-xs' onclick='destroy_room("+rooms.room+", \""+rooms.description+"\");'>destroy</button>");
+    renderVideos(rooms);
   });
-
 });
 
 socket.on('created', ({ data }) => {
